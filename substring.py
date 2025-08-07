849e6433c1ca14385603d82f3f54782773cc440d
trapboy01 <chriskantai95@gmail.com>
2025-08-07 13:37:55 +0300

    test case

--- substring.py
+++ substring.py
@@ -11,3 +11,10 @@ def length_of_longest_substring(s):
         max_len = max(max_len, right - left + 1)
 
     return max_len
+
+print(length_of_longest_substring("bbbbb"))       # Expected: 1
+print(length_of_longest_substring("pwwkew"))      # Expected: 3
+print(length_of_longest_substring(""))            # Expected: 0
+print(length_of_longest_substring("abcdef"))      # Expected: 6
+print(length_of_longest_substring("abcabcbb"))    # Expected: 3
+print(length_of_longest_substring("dvdf"))        # Expected: 3
